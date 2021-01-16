8b9adfe0a632b90caa2e2f94d1da15f841d56323
Piotr Glodek <pioglodek@gmail.com>
2021-01-16 13:04:01 +0100

    Change styles of Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,33 +6,58 @@ import {
   InputBase,
   Toolbar,
   Badge,
+  makeStyles,
 } from '@material-ui/core';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: '#FFFFFF',
+    transform: 'translateZ(0)',
+  },
+  searchInput: {
+    opacity: '0.6',
+    padding: `0 ${theme.spacing(1)}px`,
+    fontSize: '0.8rem',
+    '&:hover': {
+      backgroundColor: '#F2F2F2',
+    },
+    '& .MuiSvgIcon-root': {
+      marginRight: theme.spacing(1),
+    },
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
   return (
-    <AppBar position='static'>
+    <AppBar position='static' className={classes.root}>
       <Toolbar>
-        <Grid container>
+        <Grid container alignItems='center'>
           <Grid item>
-            <InputBase />
+            <InputBase
+              placeholder='Search'
+              className={classes.searchInput}
+              startAdornment={<SearchIcon fontSize='small' />}
+            />
           </Grid>
           <Grid item sm />
           <Grid item>
             <IconButton>
               <Badge badgeContent={4} color='secondary'>
-                <NotificationsNoneIcon />
+                <NotificationsNoneIcon fontSize='small' />
               </Badge>
             </IconButton>
             <IconButton>
               <Badge badgeContent={4} color='primary'>
-                <ChatBubbleOutlineIcon />
+                <ChatBubbleOutlineIcon fontSize='small' />
               </Badge>
             </IconButton>
             <IconButton>
-              <PowerSettingsNewIcon />
+              <PowerSettingsNewIcon fontSize='small' />
             </IconButton>
           </Grid>
         </Grid>
