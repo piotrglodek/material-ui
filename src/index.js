b3c2d1460aaf87a0d172620ff39e554d0af9a4ca
Piotr Glodek <pioglodek@gmail.com>
2021-01-16 13:03:12 +0100

    Add theme and overwrite styles

--- src/index.js
+++ src/index.js
@@ -2,14 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
 // MUI
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#3c44b126',
+      main: '#333996',
+    },
+    secondary: {
+      light: '#f8324526',
+      main: '#f83245',
+    },
+    background: {
+      default: '#f4f5fd',
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
-    </>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
